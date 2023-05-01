968e7f80a0eaae5b305d78652489aba56ccf1122
Lucas da Silveira <lu058294@intelbras.com.br>
2023-05-01 02:15:05 -0300

    refactor: change server side directory from root to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 const port = 5000;
 
-const app = express()
-app.use(bodyParser.json())
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
