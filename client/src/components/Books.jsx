0c5ac11c4595793a840da2aedba6c744a5e3d381
PoweredTable <poweredlucas@gmail.com>
2023-06-11 17:00:04 -0300

    feat: init searchbar and navbar development

---
+++ client/src/components/Books.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+ 
+
+function Books() {
+  return (
+    <h1>tela de livros</h1>
+  )
+}
+export default Books
---
+++ client/src/components/index.js
@@ -0,0 +1,10 @@
+import Home from './Home'
+import Navbar from './Navbar'
+import Books from './Books'
+import Authors from './Authors'
+export {
+    Home,
+    Navbar,
+    Books,
+    Authors
+}
