ed77d3d47398c20286b04ee26a19a517aef7d8a0
PoweredTable <poweredlucas@gmail.com>
2023-06-20 21:13:30 -0300

    feat: add routes to the server.js file

--- server/server.js
+++ server/server.js
@@ -2,10 +2,22 @@ const express = require("express");
 const cors = require('cors');
 const port = 5000;
 
+const authorRoutes = require('./routes/authors')
+const bannerRoutes = require('./routes/banners')
+const bookRoutes = require('./routes/books')
+const genreRoutes = require('./routes/genres')
+const userRoutes = require('./routes/users')
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/api/v1/authors', authorRoutes);
+app.use('/api/v1/banners', bannerRoutes);
+app.use('/api/v1/books', bookRoutes);
+app.use('/api/v1/genres', genreRoutes);
+app.use('/api/v1/users', userRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`)
