6162881cf6a5f540da3d5e5b24a3c658c3d4a431
PoweredTable <poweredlucas@gmail.com>
2023-06-28 21:52:55 -0300

    feat: implement react carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 import './App.css'
 
 import { Home, Books, Authors, Navbar } from './components'
 
 function App() {
+  // const hideNavbar = useMatch(['/admin', '/compra'].join('|'));
+  // const showAlternateNavbar = useMatch('/compra');
   return (
     <>
-    <Navbar />
-    <div className='container'> 
-      <Routes>
-        <Route path='/' element={<Home />}/> 
-        <Route path='/books' element={<Books/>}/>
-        <Route path='/authors' element={<Authors/>}/>
-      </Routes>
-    </div>
+    <Navbar></Navbar>
+      {/* {!hideNavbar && !showAlternateNavbar && !useMatch('/admin') && <Navbar />}
+      {showAlternateNavbar && <AlternateNavbar />} */}
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/books' element={<Books />} />
+          <Route path='/authors' element={<Authors />} />
+          <Route path='/*' element={<Navigate to='/' />} />
+        </Routes>
+      </div>
+ 
     </>
   )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
- 
+import React, { useState, useEffect } from 'react';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import api from '../services/api';
+
+import './Home.css'
 
 function Home() {
+  const [ banners, setBanners ] = useState([]);
+  const [ currentIndex, setCurrentIndex ] = useState(0);
+
+  useEffect(() => {
+    api.get('/api/v1/banners/all').then((response) => {
+      setBanners(response.data.result);
+    })
+    .catch((error) => {
+      console.log(error)
+    });
+  }, []);
+
   return (
-    <h1>tela principal</h1>
+    <div className='container'>
+      <Carousel  autoHeigh={true}  infiniteLoop autoPlay={true} interval={11500}
+      useKeyboardArrows={true} showStatus={false} showThumbs={false}>
+        {banners.map((banner, index) => (
+          <div className="slide" key={index}>
+            <img className='banner' src={banner.url_banner} key={index}/>
+          </div>
+        ))}
+      </Carousel>
+    </div>
   )
 }
 export default Home
