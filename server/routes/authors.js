7a0df64e5e4cc7096c22559d3c618ab5b70e1aa0
Lucas da Silveira <94416425+PoweredTable@users.noreply.github.com>
2023-07-02 20:15:57 -0300

    fix: change authors routers order

--- server/routes/authors.js
+++ server/routes/authors.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/authors');
 
-// select * from AUTORES where cod_autor = {:cod_autor};
-// usages:
-// - apresenta o autor selecionado em um modal.
-router.get('/:cod_autor', controller.getAuthorById);
-
 // select * from AUTORES
 // usages:
 // - listar os autores na página de autores.
 router.get('/all', controller.getAllAuthors);
+
+// select * from AUTORES where cod_autor = {:cod_autor};
+// usages:
+// - apresenta o autor selecionado em um modal.
+router.get('/:cod_autor', controller.getAuthorById);
 
 // INSERT INTO autores (nome, pais, url_autor) VALUES (${lista de dados})
 // usages:
