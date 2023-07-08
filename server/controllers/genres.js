f93e98ce14c6b1b6b19a581b4859525f6822c18a
Waldyr <waldyr.programing@gmail.com>
2023-07-07 21:42:16 -0300

    create andpoint for search genres by id

--- server/controllers/genres.js
+++ server/controllers/genres.js
@@ -12,3 +12,17 @@ exports.getAllGenres = (req, res) => {
         }
     )
 };
+
+exports.getGenresbyId = (req, res) => {
+    const query = "select * from generos where cod_genero = $1";
+    const values = [req.params.cod_genero];
+
+    database.query(query, values).then(
+        (result) => {
+            res.status(200).send({ result: result.rows })
+        },
+        (error) => {
+            res.status(404).send({ error: error })
+        }
+    )
+};
