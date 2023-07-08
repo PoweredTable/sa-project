aa027483675b31a81c7c6d3db3747bd632f2c2b8
Waldyr <waldyr.programing@gmail.com>
2023-07-07 21:39:42 -0300

    create routes and navigate for screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,20 @@ import { Routes, Route } from "react-router-dom"
 
 import './App.css'
 
-import { Home, Books, Authors, Navbar } from './components'
+import { Home, Books, Authors, Navbar, ViewBook } from './components'
 
 function App() {
   return (
     <>
-    <Navbar />
-    <div className='container'> 
-      <Routes>
-        <Route path='/' element={<Home />}/> 
-        <Route path='/books' element={<Books/>}/>
-        <Route path='/authors' element={<Authors/>}/>
-      </Routes>
-    </div>
+      <Navbar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/books/:id' element={<ViewBook/>}/>
+          <Route path='/books' element={<Books/>} />
+          <Route path='/authors' element={<Authors />} />
+        </Routes>
+      </div>
     </>
   )
 }
