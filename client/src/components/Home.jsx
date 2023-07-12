0564453999681897385c6308f51aeb4ea5c3058b
PoweredTable <poweredlucas@gmail.com>
2023-07-12 05:41:35 -0300

    style: remove overwriting css classnames at Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,28 +8,27 @@ import './Home.css'
 
 function Home() {
   const [ banners, setBanners ] = useState([]);
-  const [ currentIndex, setCurrentIndex ] = useState(0);
 
   useEffect(() => {
     api.get('/api/v1/banners/all').then((response) => {
       setBanners(response.data.result);
     })
-    .catch((error) => {
-      console.log(error)
-    });
+      .catch((error) => {
+        console.log(error)
+      });
   }, []);
 
   return (
-    <div className='container'>
-      <Carousel  autoHeigh={true}  infiniteLoop autoPlay={true} interval={11500}
-      useKeyboardArrows={true} showStatus={false} showThumbs={false}>
+    <>
+      <Carousel autoHeigh={true} infiniteLoop autoPlay={true} interval={11500}
+        useKeyboardArrows={true} showStatus={false} showThumbs={false} className='carousel-home'>
         {banners.map((banner, index) => (
           <div className="slide" key={index}>
-            <img className='banner' src={banner.url_banner} key={index}/>
+            <img className='banner' src={banner.url_banner} key={index} />
           </div>
         ))}
       </Carousel>
-    </div>
+    </>
   )
 }
 export default Home
